f1c6ea558c9b0bf5dba0d71a7cf49bbc7a6aab81
Jonathan Baltazar <jonrules@gmail.com>
2015-04-14 11:01:58 -0500

    no-autoship radio button

--- woocommerce-autoship/templates/product/autoship-options.php
+++ woocommerce-autoship/templates/product/autoship-options.php
@@ -31,15 +31,31 @@
 					<?php if ( $days < $autoship_min_frequency || $days > $autoship_max_frequency ) continue; ?>
 					<div class="wc-autoship-frequency-radio radio">
 						<label for="wc_autoship_frequency_<?php echo esc_html( $days ); ?>">
-							<input type="radio" name="wc_autoship_frequency"
+							<input type="radio" name="wc_autoship_frequency" class="wc-autoship-frequency-input-radio"
 								id="wc_autoship_frequency_<?php echo esc_html( $days ); ?>" 
 								value="<?php echo esc_html( $days ); ?>" /> 
 							<?php echo esc_html( $name ), ' ', __( "(Every $days days)", 'wc-autoship' ); ?>
 						</label>
 					</div>
 				<?php endforeach; ?>
+				<div id="wc-autoship-frequency-radio-no-autoship" class="wc-autoship-frequency-radio radio" style="display: none">
+					<label for="wc_autoship_frequency_<?php echo esc_html( $days ); ?>">
+						<input type="radio" name="wc_autoship_frequency" class="wc-autoship-frequency-input-radio"
+							id="wc_autoship_frequency_no_autoship" 
+							value="" /> 
+						<?php echo __( "No auto-ship. Make this a one-time purchase.", 'wc-autoship' ); ?>
+					</label>
+				</div>
 			</div>
 		</fieldset>
 	</div>
 
 </div>
+
+<script>
+jQuery(function ($) {
+	$('.wc-autoship-frequency-input-radio').click(function () {
+		$('#wc-autoship-frequency-radio-no-autoship').show();
+	});
+});
+</script>
