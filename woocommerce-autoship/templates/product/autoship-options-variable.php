13f21c94b77aa3da63f657f53636c1173af7dbcc
Jonathan Baltazar <jonrules@gmail.com>
2015-05-12 12:56:51 -0500

    panel containers

--- woocommerce-autoship/templates/product/autoship-options-variable.php
+++ woocommerce-autoship/templates/product/autoship-options-variable.php
@@ -35,33 +35,34 @@
 		
 		?>
 	
-		<fieldset>
-			<legend class="wc-autoship-add-to-autoship"><?php echo __( 'Add to Auto-Ship', 'wc-autoship' ); ?></legend>
-			<h3 class="wc-autoship-price" <?php if ( empty( $autoship_price ) ) echo 'style="display:none"'; ?>><?php echo __( 'Auto-Ship price:', 'wc-autoship'); ?> <?php echo wc_price( $autoship_price ); ?></h3>
-			<p class="wc-autoship-select-frequency"><?php echo __( 'Select an Auto-Ship Frequency to add this item to auto-ship.', 'wc-autoship' ); ?></p>
-			<div class="wc-autoship-frequency wc-autoship-frequency-radio-options">
-				<?php foreach ( $frequency_options as $name => $days ): ?>
-					<?php if ( $days < $autoship_min_frequency || $days > $autoship_max_frequency ) continue; ?>
-					<div class="wc-autoship-frequency-radio radio">
-						<label for="wc_autoship_frequency_<?php echo esc_html( $days ); ?>">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<p class="wc-autoship-select-frequency"><?php echo __( 'Select an Auto-Ship Frequency to add this item to auto-ship.', 'wc-autoship' ); ?></p>
+				<h3 class="wc-autoship-price" <?php if ( empty( $autoship_price ) ) echo 'style="display:none"'; ?>><?php echo __( 'Auto-Ship price:', 'wc-autoship'); ?> <?php echo wc_price( $autoship_price ); ?></h3>
+				<div class="wc-autoship-frequency wc-autoship-frequency-radio-options">
+					<?php foreach ( $frequency_options as $name => $days ): ?>
+						<?php if ( $days < $autoship_min_frequency || $days > $autoship_max_frequency ) continue; ?>
+						<div class="wc-autoship-frequency-radio radio">
+							<label for="wc_autoship_frequency_<?php echo esc_html( $days ); ?>">
+								<input type="radio" name="wc_autoship_frequency" class="wc-autoship-frequency-input-radio"
+									id="wc_autoship_frequency_<?php echo esc_html( $days ); ?>" 
+									value="<?php echo esc_html( $days ); ?>"
+									<?php echo checked( $days, $autoship_default_frequency ); ?> /> 
+								<?php echo esc_html( $name ), ' ', __( "(Every $days days)", 'wc-autoship' ); ?>
+							</label>
+						</div>
+					<?php endforeach; ?>
+					<div id="wc-autoship-frequency-radio-no-autoship" class="wc-autoship-frequency-radio radio" <?php if ( empty( $autoship_default_frequency ) ) echo 'style="display: none"'; ?>>
+						<label for="wc_autoship_frequency_no_autoship">
 							<input type="radio" name="wc_autoship_frequency" class="wc-autoship-frequency-input-radio"
-								id="wc_autoship_frequency_<?php echo esc_html( $days ); ?>" 
-								value="<?php echo esc_html( $days ); ?>"
-								<?php echo checked( $days, $autoship_default_frequency ); ?> /> 
-							<?php echo esc_html( $name ), ' ', __( "(Every $days days)", 'wc-autoship' ); ?>
+								id="wc_autoship_frequency_no_autoship" 
+								value="" /> 
+							<?php echo __( "No auto-ship. Make this a one-time purchase.", 'wc-autoship' ); ?>
 						</label>
 					</div>
-				<?php endforeach; ?>
-				<div id="wc-autoship-frequency-radio-no-autoship" class="wc-autoship-frequency-radio radio" <?php if ( empty( $autoship_default_frequency ) ) echo 'style="display: none"'; ?>>
-					<label for="wc_autoship_frequency_no_autoship">
-						<input type="radio" name="wc_autoship_frequency" class="wc-autoship-frequency-input-radio"
-							id="wc_autoship_frequency_no_autoship" 
-							value="" /> 
-						<?php echo __( "No auto-ship. Make this a one-time purchase.", 'wc-autoship' ); ?>
-					</label>
 				</div>
 			</div>
-		</fieldset>
+		</div>
 	</div>
 	
 </div>
